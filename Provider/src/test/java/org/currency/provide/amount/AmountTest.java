c8c2f0900d468a5268ec35c6f426af871d3ba278
Lukas <lukas.ikvm1@gmail.com>
2022-04-21 13:23:28 +0200

    Add from sek to usd exchange test with amount method

--- Provider/src/test/java/org/currency/provide/amount/AmountTest.java
+++ Provider/src/test/java/org/currency/provide/amount/AmountTest.java
@@ -1,5 +1,6 @@
 package org.currency.provide.amount;
 
+import org.currency.provide.sek.ToUSD;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -9,7 +10,16 @@ public class AmountTest {
     @Test
     void AmountReturnsCorrectValue(){
         Amount a = new Amount();
-
         assertThat(  a.amount(200)).isEqualTo(200);
+    }
+
+    @Test
+    void FromSEKToUSDExchangeCorrectlyWithAmountMethod(){
+        Amount sek = new Amount();
+        ToUSD usd = new ToUSD();
+        double money = sek.amount(10)*usd.Rate().get("USD");
+
+        assertThat(money).isEqualTo(1.0617149);
+
     }
 }
