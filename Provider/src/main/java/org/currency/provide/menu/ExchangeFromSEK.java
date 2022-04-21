b1af9ea078a631d7b7f0cc22f6eaa486cd4b5000
Lukas <lukas.ikvm1@gmail.com>
2022-04-21 15:28:15 +0200

    Update can now set amount of money and convert it

--- Provider/src/main/java/org/currency/provide/menu/ExchangeFromSEK.java
+++ Provider/src/main/java/org/currency/provide/menu/ExchangeFromSEK.java
@@ -1,6 +1,8 @@
 package org.currency.provide.menu;
 
 import org.currency.menu.Command;
+import org.currency.provide.amount.Amount;
+import org.currency.provide.convert.CurrencyConverter;
 import org.currency.provide.exchange.SEK;
 
 import java.util.Scanner;
@@ -9,19 +11,40 @@ public class ExchangeFromSEK implements Command {
 
 
     private static double currentRate = 0;
+    private static double currentAmount = 0;
+
+    private static double convertedAmount = 0;
+
+    private final SEK exchange = new SEK();
+    private final Amount sek = new Amount();
+
+    private final CurrencyConverter converter = new CurrencyConverter();
+
     private final Scanner scanner = new Scanner(System.in);
 
-    private final Command[] commands = new Command[3];
+    private final Command[] commands = new Command[4];
     public ExchangeFromSEK(){
         commands[1] = this::exchangeRate;
-        commands[2] = () -> System.out.println(currentRate);
+        commands[2] = this::setAmount;
+        commands[3] = this::convert;
         commands[0] = this::shutdown;
     }
 
+    private void convert(){
+        convertedAmount = converter.convert(currentAmount,currentRate);
+    }
+
     private void exchangeRate(){
-        String rate = scanner.next();
-        SEK sek = new SEK();
-        currentRate = sek.rate().get(rate);
+        String currency = scanner.next();
+        currentRate = exchange.rate().get(currency);
+        convertedAmount = 0;
+    }
+
+    private void setAmount(){
+        double value = scanner.nextDouble();
+        currentAmount = value;
+        sek.amount(value);
+        convertedAmount = 0;
     }
 
     public void shutdown(){
@@ -31,12 +54,22 @@ public class ExchangeFromSEK implements Command {
         System.out.println("----SEK----");
         System.out.println("1.CURRENCY");
         System.out.println("2.AMOUNT");
-        System.out.println("3.CONVERT");
+        System.out.println("3.CONVERT ");
     }
+
+    private void exchangeMenu(){
+        System.out.print("CURRENT EXCHANGE:");
+        exchange.rate().entrySet().stream().filter(e-> e.getValue().equals(currentRate)).forEach(System.out::print);
+        System.out.println("");
+        System.out.println("CURRENT AMOUNT:"+ currentAmount);
+        System.out.println("Result: " + convertedAmount + " USD");
+    }
+
     private void run() {
         int choice = 0;
         do {
             printMenuOption();
+            exchangeMenu();
             choice = readChoice(scanner);
             executeChoice(choice);
         } while (choice != 0);
